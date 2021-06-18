bc77f336ec24eb0ef29f953b115386ae9bb1b45d
kroniz-utab <adehf12@gmail.com>
2021-06-18 20:41:21 +0700

    cron job scheduler update

--- dashboard/CronjobHandler/email_test.py
+++ dashboard/CronjobHandler/email_test.py
@@ -8,22 +8,14 @@ def html_email():
     htmly = get_template('bee_template.html')
 
     today = datetime.today().strftime('%d-%m-%Y %H:%M')
-    subject = f'Laporan Kondisi AWS {today}'
+    subject = f'Laporan Kondisi AWS {today} Dari Local'
+    message = 'local test'
 
     recipient_list = [
         'adehf12@gmail.com', 
     ]
 
     # get data from model
-    last_data = [
-        Temperature.objects.order_by('-id')[0],
-        Humidity.objects.order_by('-id')[0],
-        Pressure.objects.order_by('-id')[0],
-        SolarRadiation.objects.order_by('-id')[0],
-        WindSpeed.objects.order_by('-id')[0],
-        WindDir.objects.order_by('-id')[0],
-        Precipitaion.objects.order_by('-id')[0],
-    ]
 
     context = {
         'date': today,
@@ -32,6 +24,6 @@ def html_email():
 
     html_content = htmly.render(context)
 
-    msg = EmailMultiAlternatives(subject=subject, body=html_content, from_email=settings.EMAIL_HOST_USER, to=recipient_list)
+    msg = EmailMultiAlternatives(subject=subject, body=message, from_email=settings.EMAIL_HOST_USER, to=recipient_list)
     msg.attach_alternative(html_content, 'text/html')
     msg.send()
--- dashboard/CronjobHandler/scheduler.py
+++ dashboard/CronjobHandler/scheduler.py
@@ -1,11 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .iot_puller import coba_update
-from .email_report import html_email
+from .email_test import html_email
 
 def start():
     scheduler = BackgroundScheduler()
     # scheduler.add_job(coba_update, 'cron', minute='*/5')
-    # scheduler.add_job(html_email, 'cron', minute='*/9')
-    # scheduler.add_job(coba_update, 'cron', minute='*/10') # Job for gathering from Iot
-    # scheduler.add_job(html_email, 'cron', minute='2', hour='0') # Job for send email report
+    # scheduler.add_job(html_email, 'cron', minute='*/5')
+    # scheduler.add_job(coba_update, 'cron', minute='2-52/10') # Job for gathering from Iot
+    # scheduler.add_job(html_email, 'cron', minute='53', hour='23') # Job for send email report
     scheduler.start()
