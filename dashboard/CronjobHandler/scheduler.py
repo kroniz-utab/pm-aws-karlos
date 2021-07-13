4887becf762a6da8e39ce7577434f820959675b3
kroniz-utab <adehf12@gmail.com>
2021-07-13 16:17:48 +0700

    deleting some unuseable file

--- dashboard/CronjobHandler/scheduler.py
+++ dashboard/CronjobHandler/scheduler.py
@@ -1,11 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .iot_puller import coba_update
 from .email_test import html_email
+from .full_job import testing
 
 def start():
     scheduler = BackgroundScheduler()
     # scheduler.add_job(coba_update, 'cron', minute='*/5')
-    # scheduler.add_job(html_email, 'cron', minute='*/5')
+    # scheduler.add_job(html_email, 'cron', minute='*/2')
     # scheduler.add_job(coba_update, 'cron', minute='2-52/10') # Job for gathering from Iot
     # scheduler.add_job(html_email, 'cron', minute='53', hour='23') # Job for send email report
+    scheduler.add_job(testing, 'cron', day=13, month=7, hour=12)
     scheduler.start()
