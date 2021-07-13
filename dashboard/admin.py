a97b652abe21c15c24f7765cdc56f8ff47eef7b8
kroniz-utab <adehf12@gmail.com>
2021-07-13 20:33:26 +0700

    add admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Temperature)
+class TempAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(Humidity)
+class HumAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(Pressure)
+class PressAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(WindSpeed)
+class WsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(WindDir)
+class WdAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(SolarRadiation)
+class SrAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(Precipitaion)
+class ChAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'value', 'status', 'pred_condition')
+    list_filter = ('created_at', 'status')
+
+
+@admin.register(AwsData)
+class AwsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'awsTime', 'temp', 'rh', 'press', 'solrad', 'winddir', 'windspeed', 'ch')
+    list_filter = ('created_at', )
