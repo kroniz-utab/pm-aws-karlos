515105c8e609d4046e059e139a24f6216a4bcf72
kroniz-utab <adehf12@gmail.com>
2021-07-14 10:52:53 +0700

    fix cron job

--- dashboard/CronjobHandler/full_job.py
+++ dashboard/CronjobHandler/full_job.py
@@ -6,14 +6,11 @@ from .create_new import from_dataframe
 
 def testing():
     data = pd.read_csv('data/data_januari.csv')
-    data = data.reset_index(drop=True)
-    for i in range(len(data)):
-        aws_count = AwsData.objects.count()
-        awstime, temp, rh, pa, sr, ws, wd, ch = from_dataframe(data, i)
-        if aws_count < 18:
-            no_all(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
-        elif aws_count < 144 and aws_count >= 18:
-            just_class(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
-        else:
-            full_system(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
-    
+    idx = AwsData.objects.count()
+    awstime, temp, rh, pa, sr, ws, wd, ch = from_dataframe(data, idx)
+    if idx < 18:
+        no_all(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
+    elif idx < 144 and idx >= 18:
+        just_class(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
+    else:
+        full_system(awstime, temp, rh, pa, sr, ws, wd, ch, is_rainy=1)
--- dashboard/CronjobHandler/scheduler.py
+++ dashboard/CronjobHandler/scheduler.py
@@ -3,11 +3,16 @@ from .iot_puller import coba_update
 from .email_test import html_email
 from .full_job import testing
 
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(job_defaults=job_defaults)
     # scheduler.add_job(coba_update, 'cron', minute='*/5')
     # scheduler.add_job(html_email, 'cron', minute='*/2')
     # scheduler.add_job(coba_update, 'cron', minute='2-52/10') # Job for gathering from Iot
     # scheduler.add_job(html_email, 'cron', minute='53', hour='23') # Job for send email report
-    scheduler.add_job(testing, 'cron', day=13, month=7, hour=15)
+    scheduler.add_job(testing, 'cron', minute='*/1')
     scheduler.start()
